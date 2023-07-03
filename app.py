d0a1900e95a18fba9d66d834f2e04ccf826eb9fe
Saimun Hassan <saimun4u@gmail.com>
2023-07-02 22:24:43 -0400

    Scrape data using ID and Xpath

--- app.py
+++ app.py
@@ -2,9 +2,14 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 
 path = "C:\\Users\Saimun\\Desktop\\chromedriver.exe"
-googleURL = "https://www.google.com"
+# googleURL = "https://www.google.com"
+youtubeURL = "https://www.youtube.com/@freecodecamp"
 
 # Execute the driver and open the path
 
 driver = webdriver.Chrome(executable_path = path)
-driver.get(googleURL)
+driver.get(youtubeURL)
+
+subCount = driver.find_element(By.ID, "subscriber-count")
+
+print(subCount.text)

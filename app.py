353fa8505e9b162312e937c468fe28906ae00bed
Saimun Hassan <saimun4u@gmail.com>
2023-07-02 22:18:13 -0400

    Get the no. of sub. from site using find element

--- app.py
+++ app.py
@@ -3,3 +3,8 @@ from selenium.webdriver.common.by import By
 
 path = "C:\\Users\Saimun\\Desktop\\chromedriver.exe"
 googleURL = "https://www.google.com"
+
+# Execute the driver and open the path
+
+driver = webdriver.Chrome(executable_path = path)
+driver.get(googleURL)
